8f35b605237fb32741a1f3c97f37e9c3e1e222ba
controls <eric.antonio.quintero@gmail.com>
2015-06-03 19:22:40 -0700

    The script automatic_filter_check has been introduced, this code automates Den's code to check any digital filter for noise. the foton file has been used to get all the filter names in LSC. Some futher improvements in the code shall be done later. (For zero data, no plot and also the connection breaks down after some time (Invalid IP address, java exception error

--- plot_psd.m
+++ plot_psd.m
@@ -16,7 +16,7 @@ function plot_psd(signal, output_df2, output_bqf, noise_df2, noise_bqf, channel,
     [psd_noise_bqf,~] = pwelch(noise_bqf, hanning(nfft), 3*nfft/4, nfft, fs);
       
     disp('Plot the result...');
-    figure('Position', [1 1 1200 800]);
+    figure('Position', [1 1 1200 800],'Visible','on');
     loglog(f, sqrt(psd_input), 'c', f, sqrt(psd_output_df2), 'b--', f, sqrt(psd_output_bqf), 'g--', ...
         f, sqrt(psd_noise_df2), 'r-.', f, sqrt(psd_noise_bqf), 'm-.', 'LineWidth', 3);
     grid on;
@@ -26,7 +26,7 @@ function plot_psd(signal, output_df2, output_bqf, noise_df2, noise_bqf, channel,
     set(hLegend,'FontSize', 14, 'Location', 'SouthWest');
     set(gca, 'FontSize', 16);
     axis tight;
-   
+    saveas(gcf,'PSD_Plot','jpg');
 %     orient portrait
 %     set(gcf,'Position', [1 1  1500  1200]);
 %     print(gcf, '-dpng',  [channel '.png']);

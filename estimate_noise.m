4cf2d9acac6c8315a95f36d0a508692ebf0c3850
ayush9pandey <ayush.9.pandey@gmail.com>
2015-06-22 21:55:13 -0700

    The segmentation fault previously was due to array declaration in the stack was leading to stack overflow because the signal at 16k and 32 seconds samplings time was causing the data in the txt file to be of more than 7 MB. Hence, allocated the arrays on heap and the program is now finally working successfully even with the downloaded data at 32 sec and 16kHz itself. The new finding is that, there is no difference at all in the outputs at long double and double precision for the LSC-POP110_I_IN1 filter. This means that the noise is coming out to be zero overall (the difference between long double and double).I am getting the plots corresponding to the same. To confirm of this fact, I included single precision as well in the code. So, now the code calculates outputs for double, long double and single precisons. The difference in single precision was observable and their was some finite value of the noise. Will check with more filters, tomorrow.

--- estimate_noise.m
+++ estimate_noise.m
@@ -1,7 +1,7 @@
 function [output_df2, output_bqf, noise_df2, noise_bqf] = estimate_noise(signal, iir)
 
-    n_iir = length(iir);
-    
+    n_iir = length(iir)
+    float_double=1e-7;
     
     % Calculate Biquad filter coefficients
     for i=1:n_iir
@@ -16,16 +16,23 @@ function [output_df2, output_bqf, noise_df2, noise_bqf] = estimate_noise(signal,
     output_df2 = signal;
     output_bqf_single = signal;
     output_bqf = signal;
-
+    
+    iir(1).sos
     % Calculate output from the filter bank using df2 and bqf
     for i=1:n_iir
-        output_df2_single = calculate_noise(mp(output_df2_single), iir(i).sos, iir(i).g, 0);
-        output_df2 = calculate_noise(double(output_df2), iir(i).sos, iir(i).g, 1);
-        output_bqf_single = calculate_noise(mp(output_bqf_single), iir(i).sos_bqf, iir(i).g, 2);
-        output_bqf = calculate_noise(double(output_bqf), iir(i).sos_bqf, iir(i).g, 3);
+        output_df2_single = calculate_noise(output_df2_single, iir(i).sos, iir(i).g, 0);
+        output_df2 = calculate_noise(output_df2, iir(i).sos, iir(i).g, 1);
+        output_bqf_single = calculate_noise(output_bqf_single, iir(i).sos_bqf, iir(i).g, 2);
+        output_bqf = calculate_noise(output_bqf, iir(i).sos_bqf, iir(i).g, 3);
     end
        
     % Calculate filter noise
-    mp(noise_df2,100) = (output_df2_single - output_df2);
-    mp(noise_bqf,100) = (output_bqf_single - output_bqf);
+    noise_df2= float_double*(output_df2_single - output_df2);
+    noise_bqf = float_double*(output_bqf_single - output_bqf);
+%     length(output_df2_single)
+%     length(output_df2)
+%     length(output_bqf_single)
+%     length(output_bqf)
+%     length(noise_df2)
+%     length(noise_bqf)
 end
